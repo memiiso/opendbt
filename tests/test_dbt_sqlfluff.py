1fd040f31790956284fe0470fe0eabe03f2b9a55
ismail simsek <6005685+ismailsimsek@users.noreply.github.com>
2024-09-09 15:10:39 +0200

    Integrate sqlfluff as dbt cli command

---
+++ tests/test_dbt_sqlfluff.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+from unittest import TestCase
+
+from opendbt import OpenDbtProject
+
+
+class TestDbtDocs(TestCase):
+    RESOURCES_DIR = Path(__file__).parent.joinpath("resources")
+    DBTTEST_DIR = RESOURCES_DIR.joinpath("dbttest")
+
+    def test_run_sqlfluff_lint(self):
+        dp = OpenDbtProject(project_dir=self.DBTTEST_DIR, profiles_dir=self.DBTTEST_DIR)
+        dp.run(command="sqlfluff", args=['lint'])
+
+    def test_run_sqlfluff_fix(self):
+        dp = OpenDbtProject(project_dir=self.DBTTEST_DIR, profiles_dir=self.DBTTEST_DIR)
+        dp.run(command="sqlfluff", args=['fix'])
