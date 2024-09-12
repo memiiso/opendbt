7ed7007f14fd9e8ea8cb4e42a54d974a8082157e
ismail simsek <6005685+ismailsimsek@users.noreply.github.com>
2024-09-12 08:57:25 +0200

    Integrate sqlfluff as dbt cli command

--- opendbt/dbt/v17/task/sqlfluff.py
+++ opendbt/dbt/v17/task/sqlfluff.py
@@ -7,6 +7,7 @@ from dbt.contracts.results import (
     CatalogArtifact, RunExecutionResult,
 )
 from dbt.task.compile import CompileTask
+from sqlfluff.cli import commands
 from sqlfluff.core import Linter, FluffConfig
 
 
@@ -39,7 +40,14 @@ class SqlFluffTasks(CompileTask):
 
     def fix(self) -> CatalogArtifact:
         os.chdir(self.config.project_root)
-        lint_result = self.linter.lint_paths(paths=(self.config.project_root,), fix=True, apply_fixes=True)
+        # lint_result = commands.fix(paths=(self.config.project_root))
+        # lint_result = self.linter.lint_paths(paths=(self.config.project_root,), fix=True, apply_fixes=True)
+        # Instantiate the linter
+        lnt, formatter = commands.get_linter_and_formatter(self.sqlfluff_config)
+        # Dispatch the detailed config from the linter.
+        lint_result = formatter.dispatch_config(lnt)
+        lnt.fix(formatter=formatter)
+
         return self.return_result(lint_result=lint_result)
 
     def return_result(self, lint_result):
@@ -49,7 +57,6 @@ class SqlFluffTasks(CompileTask):
             print("SqlFluff Linting Errors")
             print("\n".join([str(item) for item in lint_result.as_records()]))
         return self.results
-
 
     @classmethod
     def interpret_results(self, results: Optional[CatalogResults]) -> bool:
