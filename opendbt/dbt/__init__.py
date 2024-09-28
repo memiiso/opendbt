be1e3efbd58dca4d8b7f0f5e0e863ac0a5663abf
ismail simsek <6005685+ismailsimsek@users.noreply.github.com>
2024-09-28 09:17:58 +0200

    Integrate sqlfluff as dbt cli command

--- opendbt/dbt/__init__.py
+++ opendbt/dbt/__init__.py
@@ -1,16 +1,16 @@
 import dbt
 from packaging.version import Version
 
-import opendbt.dbt.v17.cli.main
 
 
 def patch_dbt():
     # ================================================================================================================
-    # Monkey Patching! Override dbt lib AdapterContainer.register_adapter method with new one above
+    # Monkey Patching! Override dbt lib code with new one
     # ================================================================================================================
     if Version(dbt.version.get_installed_version().to_version_string(skip_matcher=True)) < Version("1.8.0"):
         from opendbt.dbt.v17.task.docs.generate import OpenDbtGenerateTask
         from opendbt.dbt.v17.adapters.factory import OpenDbtAdapterContainer
+        import opendbt.dbt.v17.cli.main
         dbt.cli.main.sqlfluff = opendbt.dbt.v17.cli.main.sqlfluff
         dbt.cli.main.sqlfluff_lint = opendbt.dbt.v17.cli.main.sqlfluff_lint
         dbt.cli.main.sqlfluff_fix = opendbt.dbt.v17.cli.main.sqlfluff_fix
