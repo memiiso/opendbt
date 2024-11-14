988ee12b6865a38da3aff0ffc4f53bcf08febc98
ismail simsek <6005685+ismailsimsek@users.noreply.github.com>
2024-11-14 17:13:00 +0100

    Print adapter response

--- opendbt/dbt/__init__.py
+++ opendbt/dbt/__init__.py
@@ -9,22 +9,22 @@ def patch_dbt():
     dbt_version = Version(dbt.version.get_installed_version().to_version_string(skip_matcher=True))
     if dbt_version >= Version("1.7.0") and dbt_version < Version("1.8.0"):
         from opendbt.dbt.v17.task.docs.generate import OpenDbtGenerateTask
-        from opendbt.dbt.v17.adapters.factory import OpenDbtAdapterContainer
         dbt.task.generate.GenerateTask = OpenDbtGenerateTask
+        from opendbt.dbt.v17.adapters.factory import OpenDbtAdapterContainer
         dbt.adapters.factory.FACTORY = OpenDbtAdapterContainer()
     elif dbt_version >= Version("1.8.0") and dbt_version < Version("1.9.0"):
         from opendbt.dbt.v18.task.docs.generate import OpenDbtGenerateTask
-        from opendbt.dbt.v18.adapters.factory import OpenDbtAdapterContainer
         dbt.task.docs.generate.GenerateTask = OpenDbtGenerateTask
+        from opendbt.dbt.v18.adapters.factory import OpenDbtAdapterContainer
         dbt.adapters.factory.FACTORY = OpenDbtAdapterContainer()
+        from opendbt.dbt.v18.task.run import ModelRunner
+        dbt.task.run.ModelRunner = ModelRunner
     else:
         raise Exception(
             f"Unsupported dbt version {dbt_version}, please make sure dbt version is supported/integrated by opendbt")
 
     # shared code patches
     import opendbt.dbt.shared.cli.main
-    import opendbt.dbt.shared.task.run
     dbt.cli.main.sqlfluff = opendbt.dbt.shared.cli.main.sqlfluff
     dbt.cli.main.sqlfluff_lint = opendbt.dbt.shared.cli.main.sqlfluff_lint
     dbt.cli.main.sqlfluff_fix = opendbt.dbt.shared.cli.main.sqlfluff_fix
-    dbt.task.run.ModelRunner = opendbt.dbt.shared.task.run.ModelRunner
