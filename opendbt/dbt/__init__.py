2c4a3d8d10fa80f889a552f9ecbf52fa58b3c8a5
ismail simsek <6005685+ismailsimsek@users.noreply.github.com>
2024-11-14 17:21:42 +0100

    Log detailed Adapter execution information (#29)
    * Print adapter response
    
    * Print adapter response
    
    * Print adapter response

--- opendbt/dbt/__init__.py
+++ opendbt/dbt/__init__.py
@@ -9,14 +9,18 @@ def patch_dbt():
     dbt_version = Version(dbt.version.get_installed_version().to_version_string(skip_matcher=True))
     if dbt_version >= Version("1.7.0") and dbt_version < Version("1.8.0"):
         from opendbt.dbt.v17.task.docs.generate import OpenDbtGenerateTask
-        from opendbt.dbt.v17.adapters.factory import OpenDbtAdapterContainer
         dbt.task.generate.GenerateTask = OpenDbtGenerateTask
+        from opendbt.dbt.v17.adapters.factory import OpenDbtAdapterContainer
         dbt.adapters.factory.FACTORY = OpenDbtAdapterContainer()
+        from opendbt.dbt.v17.task.run import ModelRunner
+        dbt.task.run.ModelRunner = ModelRunner
     elif dbt_version >= Version("1.8.0") and dbt_version < Version("1.9.0"):
         from opendbt.dbt.v18.task.docs.generate import OpenDbtGenerateTask
-        from opendbt.dbt.v18.adapters.factory import OpenDbtAdapterContainer
         dbt.task.docs.generate.GenerateTask = OpenDbtGenerateTask
+        from opendbt.dbt.v18.adapters.factory import OpenDbtAdapterContainer
         dbt.adapters.factory.FACTORY = OpenDbtAdapterContainer()
+        from opendbt.dbt.v18.task.run import ModelRunner
+        dbt.task.run.ModelRunner = ModelRunner
     else:
         raise Exception(
             f"Unsupported dbt version {dbt_version}, please make sure dbt version is supported/integrated by opendbt")
---
+++ opendbt/dbt/v17/task/run.py
@@ -0,0 +1,34 @@
+from dbt.contracts.results import NodeStatus
+from dbt.events.base_types import EventLevel
+from dbt.events.functions import fire_event
+from dbt.events.types import (
+    LogModelResult,
+)
+from dbt.task import run
+
+
+class ModelRunner(run.ModelRunner):
+
+    def print_result_adapter_response(self, result):
+        if hasattr(result, 'adapter_response'):
+            if result.status == NodeStatus.Error:
+                status = result.status
+                level = EventLevel.ERROR
+            else:
+                status = result.message
+                level = EventLevel.INFO
+            fire_event(
+                LogModelResult(
+                    description=str(result.adapter_response),
+                    status=status,
+                    index=self.node_index,
+                    total=self.num_nodes,
+                    execution_time=result.execution_time,
+                    node_info=self.node.node_info,
+                ),
+                level=level,
+            )
+
+    def print_result_line(self, result):
+        super().print_result_line(result)
+        self.print_result_adapter_response(result=result)
