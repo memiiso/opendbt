b3591d751918f6218b49bf6e29aa9b6599ee15b2
ismail simsek <6005685+ismailsimsek@users.noreply.github.com>
2024-12-16 21:33:44 +0100

    Fix dbt docs return value

--- opendbt/dbt/__init__.py
+++ opendbt/dbt/__init__.py
@@ -1,4 +1,5 @@
 import dbt
+from dbt import version
 from packaging.version import Version
 
 
@@ -6,7 +7,7 @@ def patch_dbt():
     # ================================================================================================================
     # Monkey Patching! Override dbt lib code with new one
     # ================================================================================================================
-    dbt_version = Version(dbt.version.get_installed_version().to_version_string(skip_matcher=True))
+    dbt_version = Version(version.get_installed_version().to_version_string(skip_matcher=True))
     if Version("1.6.0") <= dbt_version < Version("1.8.0"):
         from opendbt.dbt.v17.task.docs.generate import OpenDbtGenerateTask
         dbt.task.generate.GenerateTask = OpenDbtGenerateTask
