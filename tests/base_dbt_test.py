580769fa16508f42ed08d7eaff95d0bba59d2b93
ismail simsek <6005685+ismailsimsek@users.noreply.github.com>
2024-12-17 12:28:28 +0100

    Add dbt mesh to main page and improve tests (#52)
    * Tests run dbt clean before tests
    
    * Tests run dbt clean before tests

--- tests/base_dbt_test.py
+++ tests/base_dbt_test.py
@@ -3,9 +3,18 @@ from unittest import TestCase
 
 from dbt.version import get_installed_version as get_dbt_version
 
+from opendbt import OpenDbtCli
+
 
 class BaseDbtTest(TestCase):
     RESOURCES_DIR = Path(__file__).parent.joinpath("resources")
     DBTCORE_DIR = RESOURCES_DIR.joinpath("dbtcore")
     DBTFINANCE_DIR = RESOURCES_DIR.joinpath("dbtfinance")
     DBT_VERSION = get_dbt_version()
+
+    @classmethod
+    def setUpClass(cls):
+        dpf = OpenDbtCli(project_dir=BaseDbtTest.DBTFINANCE_DIR, profiles_dir=BaseDbtTest.DBTFINANCE_DIR)
+        dpc = OpenDbtCli(project_dir=BaseDbtTest.DBTCORE_DIR, profiles_dir=BaseDbtTest.DBTCORE_DIR)
+        dpf.invoke(args=["clean"])
+        dpc.invoke(args=["clean"])
