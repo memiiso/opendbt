b68b4c6d9c66623efa0a1f934d8068174249152e
ismail simsek <6005685+ismailsimsek@users.noreply.github.com>
2025-04-21 16:37:55 +0200

    Align class naming

--- opendbt/dbt/__init__.py
+++ opendbt/dbt/__init__.py
@@ -16,14 +16,14 @@ try:
         from opendbt.dbt.v17.adapters.factory import OpenDbtAdapterContainer
         from opendbt.dbt.v17.task.docs.generate import OpenDbtGenerateTask
         from opendbt.dbt.v17.config.runtime import OpenDbtRuntimeConfig
-        from opendbt.dbt.v17.task.run import ModelRunner
+        from opendbt.dbt.v17.task.run import OpenDbtModelRunner
     elif Version("1.8.0") <= dbt_version < Version("1.10.0"):
         RuntimePatcher(module_name="dbt.task.docs").patch_attribute(attribute_name="DOCS_INDEX_FILE_PATH",
                                                                     new_value=OPENDBT_INDEX_HTML_FILE)
         from opendbt.dbt.v18.adapters.factory import OpenDbtAdapterContainer
         from opendbt.dbt.v18.task.docs.generate import OpenDbtGenerateTask
         from opendbt.dbt.v18.config.runtime import OpenDbtRuntimeConfig
-        from opendbt.dbt.v18.task.run import ModelRunner
+        from opendbt.dbt.v18.task.run import OpenDbtModelRunner
     else:
         raise Exception(
             f"Unsupported dbt version {dbt_version}, please make sure dbt version is supported/integrated by opendbt")
